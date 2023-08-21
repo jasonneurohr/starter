a7a7330df0bb607778fc8171bf7d206730203ea1
Jason <jason@jasonneurohr.com>
2023-08-21 21:54:02 +1000

    updates for go config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,7 +26,135 @@ require("lazy").setup({
         },
       },
     },
-    { import = "lazyvim.plugins.extras.lang.go" },
+    {
+      import = "lazyvim.plugins.extras.lang.go",
+      opts = function(_, opts)
+        vim.list_extend(opts.ensure_installed, {
+          "go",
+          "gomod",
+          "gowork",
+          "gosum",
+        })
+      end,
+    },
+    -- adapted from here https://github.com/bjartek/dotfiles/blob/master/.config/lazyvim/lua/plugins/go.lua
+    {
+      "ray-x/go.nvim",
+      dependencies = {
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+      },
+      config = function()
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          pattern = { "*.go" },
+          callback = function()
+            local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding(0))
+            params.context = { only = { "source.organizeImports" } }
+
+            local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+            for _, res in pairs(result or {}) do
+              for _, r in pairs(res.result or {}) do
+                if r.edit then
+                  vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding(0))
+                else
+                  vim.lsp.buf.execute_command(r.command)
+                end
+              end
+            end
+          end,
+        })
+      end,
+      event = "CmdlineEnter",
+      ft = { "go", "gomod", "gosum", "gowork" },
+    },
+    -- mason
+    {
+      "williamboman/mason.nvim",
+      opts = function(_, opts)
+        vim.list_extend(opts.ensure_installed, {
+          "gopls",
+          "goimports",
+          "golangci-lint",
+          "golangci-lint-langserver", -- Wraps golangci-lint as a language server
+          "goimports-reviser",
+          "delve",
+        })
+      end,
+    },
+    -- end adaption from https://github.com/bjartek/dotfiles/blob/master/.config/lazyvim/lua/plugins/go.lua
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          gopls = {
+            keys = {
+              -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
+              { "<leader>td", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
+            },
+            settings = {
+              gopls = {
+                gofumpt = true,
+                codelenses = {
+                  gc_details = false,
+                  generate = true,
+                  regenerate_cgo = true,
+                  run_govulncheck = true,
+                  test = true,
+                  tidy = true,
+                  upgrade_dependency = true,
+                  vendor = true,
+                },
+                hints = {
+                  assignVariableTypes = true,
+                  compositeLiteralFields = true,
+                  compositeLiteralTypes = true,
+                  constantValues = true,
+                  functionTypeParameters = true,
+                  parameterNames = true,
+                  rangeVariableTypes = true,
+                },
+                analyses = {
+                  fieldalignment = true,
+                  nilness = true,
+                  unusedparams = true,
+                  unusedwrite = true,
+                  useany = true,
+                },
+                usePlaceholders = true,
+                completeUnimported = true,
+                staticcheck = true,
+                directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+                semanticTokens = true,
+              },
+            },
+          },
+        },
+        setup = {
+          gopls = function(_, opts)
+            -- workaround for gopls not supporting semanticTokensProvider
+            -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+            require("lazyvim.util").on_attach(function(client, _)
+              if client.name == "gopls" then
+                if not client.server_capabilities.semanticTokensProvider then
+                  local semantic = client.config.capabilities.textDocument.semanticTokens
+                  client.server_capabilities.semanticTokensProvider = {
+                    full = true,
+                    legend = {
+                      tokenTypes = semantic.tokenTypes,
+                      tokenModifiers = semantic.tokenModifiers,
+                    },
+                    range = true,
+                  }
+                end
+              end
+            end)
+            -- end workaround
+          end,
+        },
+      },
+    },
+    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "plugins" },
   },
   defaults = {
