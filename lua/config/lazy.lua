8ec57747b13aeba400dcf35c4896f2ac3a9f2f9a
Jason Neurohr <jason.neurohr@hellostake.com.au>
2023-10-11 15:07:07 +1100

    adjust the comment color and handle the lineNR etc colors better

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,6 +11,22 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim",                          import = "lazyvim.plugins" },
+    {
+      "folke/tokyonight.nvim",
+      lazy = true,
+      opts = {
+        style = "moon",
+        on_colors = function(colors)
+          colors.comment = colors.orange
+        end,
+        on_highlights = function(highlights, colors)
+          highlights.LineNrAbove = { fg = '#51B3EC', bold = true }
+          highlights.LineNr = { fg = '#FFFFFF', bold = true }
+          highlights.CursorLineNr = { fg = '#FFFFFF', bold = true }
+          highlights.LineNrBelow = { fg = '#FB508F', bold = true }
+        end
+      },
+    },
     { import = "lazyvim.plugins.extras.lang.json" },
     {
       "nvim-treesitter/nvim-treesitter",
@@ -221,7 +237,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  --install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
